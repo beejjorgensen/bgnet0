6ca5eca2bebfeb80cbae32058b85ab8ddacaedc8
Brian "Beej Jorgensen" Hall <beej@beej.us>
2024-07-10 12:53:36 -0700

    remove old build tools

--- bin/preproc_config.py
+++
@@ -1,6 +0,0 @@
-EXAMPLE_URL = "https://beej.us/guide/bgnet0/source/examples/"
-EXERCISE_URL = "https://beej.us/guide/bgnet0/source/exercises/"
-WIKIPEDIA_URL = "https://en.wikipedia.org/wiki/"
-MAN_URL = "https://man.archlinux.org/man/"
-
-MANBREAK = "rule"
